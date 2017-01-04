8969d59722838094d2cdedf5690b73a8888a71dc
Frankie Bell <fbell.pro@gmail.com>
2017-01-04 13:44:04 +0000

    app-4 multiplication table added

--- App-4/App-4/ViewController.swift
+++ App-4/App-4/ViewController.swift
@@ -10,7 +10,14 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet weak var slider: UISlider!
 
+    @IBAction func sliderChanged(_ sender: Any) {
+        table.reloadData()
+    }
+    
+    @IBOutlet weak var table: UITableView!
+    
     // setting number of rows in table
     internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 50
@@ -21,7 +28,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
         
-        cell.textLabel?.text = String(indexPath.row + 1)
+        cell.textLabel?.text = String(Int(slider.value * 20) * (indexPath.row + 1))
         
         return cell
     }
